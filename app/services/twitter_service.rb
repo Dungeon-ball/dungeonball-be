72c0fbe7c3166c0e4b955217b8afb70c8dd5f90a
Tyler Ross <tross020890@gmail.com>
2022-06-04 18:59:55 -0500

    Feat: twitter service setup to get recent tweets

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -4,7 +4,6 @@ class TwitterService
     response = conn.get("/2/tweets/counts/recent") do |faraday|
       faraday.params[:query] = query
     end
-    binding.pry
     JSON.parse(response.body, symbolize_names: true)[:meta][:total_tweet_count]
   end
 
