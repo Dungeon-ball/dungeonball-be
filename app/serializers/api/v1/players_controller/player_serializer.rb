78cdfed8ccba2c7b7867de51b6d6e1dc5024f95a
Brylan Gannon <brylan.gannon112@gmail.com>
2022-06-07 12:52:01 -0600

    Feat: finished the find all by name endpoint

---
+++ app/serializers/api/v1/players_controller/player_serializer.rb
@@ -0,0 +1,28 @@
+class Api::V1::PlayersController::PlayerSerializer
+  def self.players_index(players_array)
+    {
+      data: players_array.map do |player_data|
+        player = player_data[:player]
+        {
+          id: player.id.to_s,
+         type: 'player',
+         attributes: {
+           name: player.name,
+           strength: player.dnd_strength,
+           dexterity: player.dexterity,
+           constitution: player.constitution,
+           intelligence: player.intelligence,
+           wisdom: player.wisdom,
+           charisma: Player.to_dnd(player_data[:recent_tweets]),
+           class: {
+              name: player_data[:class].name,
+              description: player_data[:class].description,
+              hitpoints: player_data[:class].hitpoints,
+              proficiencies: player_data[:class].proficiencies
+           }
+         }
+        }
+      end
+    }
+  end
+end
