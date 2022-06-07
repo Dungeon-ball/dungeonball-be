2470ed10eb57e84ee357bdbc1a75f38c145a7786
Brylan Gannon <brylan.gannon112@gmail.com>
2022-06-06 22:46:40 -0600

    Refactor: DndService has typo in URI

--- app/services/dnd_service.rb
+++ app/services/dnd_service.rb
@@ -1,11 +1,11 @@
 class DndService
   def self.get_class_data(name)
-    response = conn.get("/classes/#{name}")
+    response = conn.get("/api/classes/#{name.downcase}")
 
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.conn
-    Faraday.new('https://www.dnd5eapi.co/api')
+    Faraday.new('https://www.dnd5eapi.co')
   end
 end
