714a9cde850b40831f1ce92a522a2c5058632761
Joseph Senecal <jasenecal2@gmail.com>
2022-06-08 00:21:52 -0600

    Test: add response shape testing

--- spec/requests/api/v1/parties/create_spec.rb
+++ spec/requests/api/v1/parties/create_spec.rb
@@ -54,13 +54,33 @@ RSpec.describe 'party creation endpoint' do
     end
   end
 
-  context 'response shape' do
-    xit 'returns ' do
+  context 'response' do
+    it 'returns the appropriate response shape' do
       player1 = create(:player)
       player2 = create(:player)
       player3 = create(:player)
       party = Party.create!(user_id: 5, name: "test name")
       post ("/api/v1/party/players?player_id=#{player1.id}&user_id=1")
+      response_body = JSON.parse(response.body)
+    binding.pry
+      expect(response_body.class).to eq Hash
+      expect(response_body[:data].class).to eq Hash
+      expect(response_body[:data].keys.include?(:id))to eq true
+      expect(response_body[:data].keys.include?(:type))to eq true
+      expect(response_body[:data][:attributes].keys.include?(:name)).to eq true
+      expect(response_body[:data][:attributes][:relationships].keys.include?(:players)).to eq true
+      expect(response_body[:data][:attributes][:relationships][:players][:data].class).to eq Array
+      unless response_body[:data][:attributes][:relationships][:players][:data].length = 0
+        expect(response_body[:data][:attributes][:relationships][:players][:data].first.class).to eq Hash
+        expect(response_body[:data][:attributes][:relationships][:players][:data].last.class).to eq Hash
+        expect(response_body[:data][:attributes][:relationships][:players][:data].first[:type]).to eq "player"
+        expect(response_body[:data][:attributes][:relationships][:players][:data].first.keys.include?(:id)).to eq true
+        expect(response_body[:data][:attributes][:relationships][:players][:data].first.keys.include?(:name)).to eq true
+      end
+      expect(response_body[:data][:attributes][:relationships][:user].class).to eq Hash
+      expect(response_body[:data][:attributes][:relationships][:user][:data].class).to eq Hash
+      expect(response_body[:data][:attributes][:relationships][:user][:data][:type]).to eq "user"
+      expect(response_body[:data][:attributes][:relationships][:user][:data][:id]).to match /\d+/
     end
   end
 
